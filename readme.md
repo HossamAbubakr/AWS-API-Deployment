8c4ebf2d4bf37697365220d57e0723266b42085c
HossamAbubakr <sam.baker990@gmail.com>
2021-05-16 04:39:28 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,61 @@
+# Image Filter API, , AWS API Deployment
+
+
+## Table of Contents
+
+* [Summary](#Summary)
+
+* [Technologies](#Technologies)
+
+* [Structure](#Structure)
+
+* [Usage](#usage)
+
+## Summary
+
+This image filter API was configured and delpoyed to AWS as part of my **AWS Cloud Developer Nanodegree from Udacity**.
+
+It demonstrates my understanding and ability to configure the following services:  
+AWS Elastic Beanstalk  
+AWS IAM User Permissions  
+AWS EB CLI (Elastic Beanstalk Command Line Interface)
+
+## Technologies
+
+Node.JS was used for the backend.  
+Typescript was used for creating the endpoints.  
+
+
+## Structure 
+```
+
++---deployment_screenshots
+|       Deployment.PNG
+|
+\---src
+    |   server.ts
+    |
+    \---util
+        |   util.ts
+
+```
+
+
+## Usage
+
+You will have to configure/use the following services to deploy the API.  
+
+1. AWS Elastic Beanstalk  
+2. AWS IAM User Permissions 
+3. AWS EB CLI (Elastic Beanstalk Command Line Interface)
+
+You can run the project locally using
+```shell
+npm run dev
+```
+You can also build the project using the included configuration settings
+```shell
+npm run build
+```
+
+
---
+++ src/server.ts
@@ -0,0 +1,44 @@
+import express from 'express';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("Welcome to the Uagram endpoint, we are still in beta but so far we have the following endpoint(s) available:\n GET: /filteredimage?image_url=URL_HERE")
+  } );
+
+  // filterImage Endpoint
+  app.get("/filteredimage", async (req, res) => {
+    let { image_url } = req.query;
+    if (!image_url || !linkChecker(image_url)) {
+      return res.status(400).send(`Invalid URL or unsupported Image format (Only JPEG/JPG and PNG are allowed)`);
+    }
+    let filteredimage: string = await filterImageFromURL(image_url);
+    // Express's sendFile has the following arguments available 
+    // res.sendFile(fileName, options, func{})
+    // we will use the last argument to make sure the file is deleted AFTER delivery
+    res.sendFile(filteredimage, {}, () => deleteLocalFiles([filteredimage]));
+  })
+
+  // Verify the URL
+  function linkChecker(inputText: string) {
+    let urlPattern = new RegExp(
+      /^((?:https?:\/\/)?[^.\/]+(?:\.[^.\/]+)+(?:\/.*)?)\.(?:jpe?g|png)$/gi
+    );
+    return urlPattern.test(inputText);
+  }
+  
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
